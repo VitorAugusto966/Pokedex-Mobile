f37297b335cb14ad7121885621df3c69d54df1e1
VitorAugusto966 <augusto.vitor@aluno.ifsp.edu.br>
2022-12-30 18:38:05 -0300

    Projeto Pokedex

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+const urlBase = "https://pokeapi.co/api/v2/pokemon/";
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
 export class Tab1Page {
 
-  constructor() {}
+  
+  public pokemon:any = {};
+
+  constructor(private http: HttpClient) {
+    this.pokemon = null;
+    console.log(this.pokemon);
+  }
+
+  handleChange(event:any) {
+    const query = event.target.value.toLowerCase();
+
+    
+    //console.log(query);
+    //console.log(query.length);
+    if(query.length>= 3)
+    {
+      this.http.get(`${urlBase}` + query).toPromise()
+      .then((result) => {
+        this.pokemon = result
+      })
+      .catch((error)=>
+      {
+        this.pokemon = null;
+          console.log("pokemon n encontrado")
+      })
+    }
+
+    
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const urlBase = "https://pokeapi.co/api/v2/pokemon/";
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,80 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public pokes: any = [];
+  public pageAtual: any = 1;
+
+  constructor(private http: HttpClient,) {
+     this.get(this.pageAtual);
+  }
+
+  public async next() {
+    this.pokes = [];
+    console.log("Entrou")
+    this.pageAtual = this.pageAtual + 1;
+    await this.get(this.pageAtual);
+  }
+   public async previous() {
+    
+    if (this.pageAtual != 1) {
+      this.pokes = [];
+      this.pageAtual = this.pageAtual - 1;
+      await this.get(this.pageAtual);
+    }
+  }
+
+
+  async get(page: any) {
+    
+    this.pageAtual = page;
+    if (page == 1) {
+      page = 1;
+    }
+    else {
+      page = page * 20
+    }
+
+    if (this.pageAtual == 1) {
+      for (let i = page; i <= page * 20; i++) {
+        await this.http.get(`${urlBase}` + i).toPromise()
+          .then((result) => {
+            this.pokes.push(result);
+          })
+      }
+    }
+    else {
+      for (let i = page; i <= page * 2; i++) {
+        await this.http.get(`${urlBase}` + i).toPromise()
+          .then((result) => {
+            this.pokes.push(result);
+          })
+      }
+    }
+
+    console.log(this.pokes)
+
+
+  }
+
+  /* async getImage()
+   {
+     await this.get();
+ 
+    // if(this.pokes.result != undefined )
+   //  {
+       for(let poke of this.pokes.result)
+       {
+         await this.http.get(poke.url).toPromise()
+         .then((result)=>
+         {
+           console.log(result);
+           console.log("entrou");
+           this.img.push(result);
+         })
+       }
+    // }
+     
+     }*/
+
 
 }
